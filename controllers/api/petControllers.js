9e25604276546e17c25f14a65c9422b92539c832
Martyna <martynala1313@gamil.com>
2023-01-11 08:41:55 +0100

    backend przetwarzanie

--- controllers/api/petControllers.js
+++ controllers/api/petControllers.js
@@ -1,50 +1,72 @@
-const pet = require("../../db/models/pet")
+const pet = require("../../db/models/pet");
+
 
 class PetControllers {
+
+   async savePet(req, res){
+
+        const imie  = req.body.imie;
+        const gatunek  = req.body.gatunek;
+        const plec  = req.body.plec;
+        const wiek = req.body.wiek;
+        const wielkosc  = req.body.wielkosc;
+        const miasto  = req.body.miasto;
+        const stan  = req.body.stan;
+
+    
+        const Pet = new pet({imie, gatunek, plec, wiek, wielkosc, miasto, stan});
+        await Pet.save();
+
+       // res.send('Zwierzak został dodany! Informacje: ' + imie + gatunek+ plec+ wiek +wielkosc + miasto + stan);
+       res.status(201).json(Pet);
+    
+}
+async getAllPets(req, res){
+        let doc;
+        try{
+        doc = await pet.find({});
+        }catch(err){
+                return res.status(500, json({error:err.message}));
+        }
+                console.log(doc);
+                res.status(200).json(doc);
+        }
         
-   savePet(req, res){
+        
+
+async getPet(req, res){
+        const id =req.params.id;
+        const Pet = await pet.findOne({_id:id});
+        res.status(200).json(Pet);
+}
+
+async updatePet(req, res){
+        const id = req.params.id;
+        const imie  = req.body.imie;
+        const gatunek  = req.body.gatunek;
+        const plec  = req.body.plec;
+        const wiek = req.body.wiek;
+        const wielkosc  = req.body.wielkosc;
+        const miasto  = req.body.miasto;
+        const stan  = req.body.stan;
+
     
-     //   const newPet = new pet({
-               
-      //          imie: 'Jasiu',
-       //         gatunek: 'Jasiu',
-        //        plec: 'Jasiu',
-        //        wiek: 'Jasiu',
-        //        wielkosc: 'Jasiu',
-         //       miasto: 'Jasiu',
-         //       stan: 'Jasiu'
-       // });
-       // newPet.save().then(()=>{
-       //         console.log('zwierze zapisane')
-      //  });
-
-      const imie  = req.body.imie;
-      const gatunek  = req.body.gatunek;
-      const plec  = req.body.plec;
-      const wiek = req.body.wiek;
-      const wielkosc  = req.body.wielkosc;
-      const miasto  = req.body.miasto;
-      const stan  = req.body.stan;
-
-
-        res.send('Strona głóna działa!');
-    
+        const Pet = await pet.findOne({_id: id});
+        Pet.imie = imie;
+        Pet.gatunek = gatunek;
+        Pet.plec = plec;
+        Pet.wiek = wiek;
+        Pet.wielkosc = wielkosc;
+        Pet.miasto = miasto;
+        Pet.stan = stan;
+        await Pet.save();
+        res.status(201).json(Pet);
 }
-getAllPets(req, res){
-        res.send('API Działa');
-}
-getPet(req, res){
-        res.send('.info o petcie');
-}
-savePet(req, res){
-        res.send('.pet zapisany.');
-}
-updatePet(req, res){
-        res.send('.pet zaktualizowany.');
-}
-deletePet(req, res){
+async deletePet(req, res){
         const id  = req.params.id;
-        res.send('.pet usunięty.' + id );
+        await pet.deleteOne({_id: id});
+
+        res.sendStatus(204);
 }
 
 }
