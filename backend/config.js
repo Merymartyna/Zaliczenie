c914c10e882a200187575f29c929c7ac37a2d9af
Martyna Lange <martynala1313@gmail.com>
2023-01-21 12:02:35 +0100

    Update config.js

--- backend/config.js
+++ backend/config.js
@@ -2,5 +2,5 @@ require('dotenv').config();
 
 module.exports = {
     port: process.env.port,
-    database: process.env.database || 'mongodb+srv://student:student@cluster0.dsesucb.mongodb.net/zaliczenie?retryWrites=true&w=majority'
+    database: process.env.database || 'informacja poufna'
 }
